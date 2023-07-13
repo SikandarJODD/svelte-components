0170284010dcccddac1761cd9db0ae20f2528388
SikandarJODD <vu1f2021072@pvppcoe.ac.in>
2023-07-13 20:25:30 +0530

    updated

--- src/routes/c/navs/+page.svelte
+++ src/routes/c/navs/+page.svelte
@@ -3,9 +3,10 @@
 	// import N2 from '$lib/components/navs/n2.svelte';
 	// import N1 from '$lib/components/navs/n1.svelte';
 	import Box from '$lib/components/Box.svelte';
-	import { clickToCopyAction } from 'svelte-legos';
-	import { confettiAction } from 'svelte-legos';
+	// import { clickToCopyAction } from 'svelte-legos';
+	// import { confettiAction } from 'svelte-legos';
 	import { navbarsList } from '$lib/store';
+	import { copy } from 'svelte-copy';
 
 	let cls = 'badge badge-outline font-bold p-4 text-[17px] hover:bg-base-200 cursor-pointer mx-1';
 </script>
@@ -21,11 +22,7 @@
 	{#each $navbarsList as item, i}
 		<div class="flex items-start mt-3">
 			<h3 class="m-0 p-0 mr-2">{i + 1}. {item.title}</h3>
-			<button
-				class="btn btn-sm btn-outline"
-				use:clickToCopyAction={item.code}
-				use:confettiAction={{ type: 'school-pride' }}>Copy</button
-			>
+			<button class="btn btn-sm btn-outline capitalize" use:copy={item.code}>Copy</button>
 		</div>
 		<div class="not-prose">
 			<Box><svelte:component this={item.component} /></Box>
